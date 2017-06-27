d635c5300f6275c4855814069bcce10f76901f4b
N8RD <svl66@yahoo.dk>
2017-06-27 16:36:25 +0200

    nu med array

--- app/routes/galleri.js
+++ app/routes/galleri.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/galleri', function (req, res) {
-    //res.end("Forside til galleri");
+router.get('/galleri/:id', function (req, res, next) {
+    var id = req.params.id;
     res.render('galleri', {
         "pageTitle": "Galleriet",
         "pageId": "galleri",
+        "id": id
     });
 });
 
